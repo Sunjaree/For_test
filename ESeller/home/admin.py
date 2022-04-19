250e90aa4745cd28fe380d135778db5b63f36d32
Sunjaree <sunjare.zulfiker@gmail.com>
2022-04-19 11:27:32 +0600

    asa

--- ESeller/home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from home.models import Contact, Product, Sent_replies
-
-admin.site.register(Contact)
-admin.site.register(Product)
-admin.site.register(Sent_replies)
